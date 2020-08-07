d8e35c8b507c9520340a9ce48b3e9605140632af
Jill Short <jillian.b.short@gmail.com>
2020-08-07 05:19:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
+class Student < User 
+  attr_accessor :knowledge
+  
+  def initialize(knowledge)
+    @knowledge = []
+  end
+  
+  def learn 
+    
+  end
+  
+  def knowledge
+    @knowledge.self
+  end
 
 end
